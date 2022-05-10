7c461a671b3029f30175780fe2a191d4bd030536
David Vera <indigodavid@gmail.com>
2022-05-09 22:05:46 -0500

    Edit index.js to comply ESLint

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const navLinks = document.getElementById('navigation-links');
 const logo = document.getElementById('logo');
 const navAnchor = document.querySelector('.nav-link');
 
-function toggleMenu(){
+function toggleMenu() {
   menu.classList.toggle('menu-active');
   navLinks.classList.toggle('nav-active');
   logo.classList.toggle('hidden');
