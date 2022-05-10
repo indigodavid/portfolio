dfa83556bfa571522bd3e2cc744431fc99b9c795
David Vera <indigodavid@gmail.com>
2022-05-10 13:37:44 -0500

    Fix linters errors and animation

--- js/index.js
+++ js/index.js
@@ -2,17 +2,19 @@ const menu = document.getElementById('menu');
 const navLinks = document.getElementById('navigation-links');
 const logo = document.getElementById('logo');
 const navAnchor = document.querySelectorAll('.nav-link');
+const nav = document.querySelector('nav');
 
 function toggleMenu() {
   menu.classList.toggle('menu-active');
   navLinks.classList.toggle('nav-active');
   logo.classList.toggle('hidden');
+  nav.classList.toggle('active');
 }
 
 menu.addEventListener('click', toggleMenu);
 
 navAnchor.forEach(
   (navAnchors) => {
-navAnchors.addEventListener('click', toggleMenu);
-}
+    navAnchors.addEventListener('click', toggleMenu);
+  },
 );
