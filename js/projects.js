0b65266cc885ff76b9d215141361204dc152b3de
David Vera <indigodavid@gmail.com>
2022-05-12 03:38:06 -0500

    Refactor projects.js to comply linters

--- js/projects.js
+++ js/projects.js
@@ -1,162 +1,160 @@
-const projectData=[
+const projectSection = document.getElementById('projects');
+const featuredContent = document.getElementById('featured-content');
+const modal = document.getElementById('modal-container');
+const closePopup = document.getElementById('modal-close');
+const featuredLink = document.getElementById('featured-link');
+const projectData = [
   {
-    name: "Multi-Post Stories",
-    image: "./images/featured-project.png ",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    name: 'Multi-Post Stories',
+    image: './images/featured-project.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     languages: [
-      "html",
-      "css",
-      "bootstap",
-      "Ruby"
+      'html',
+      'css',
+      'bootstap',
+      'Ruby',
     ],
-    liveUrl: "https://indigodavid.github.io/portfolio/",
-    sourceUrl: "https://github.com/indigodavid/portfolio"
+    liveUrl: 'https://indigodavid.github.io/portfolio/',
+    sourceUrl: 'https://github.com/indigodavid/portfolio',
   },
   {
-    name: "Professional Art Printing Data",
-    image: "./images/placeholder-professional.png",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Professional Art Printing Data',
+    image: './images/placeholder-professional.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     languages: [
-      "html",
-      "bootstap",
-      "Ruby"
+      'html',
+      'bootstap',
+      'Ruby',
     ],
-    liveUrl: "https://indigodavid.github.io/portfolio/",
-    sourceUrl: "https://github.com/indigodavid/portfolio"
+    liveUrl: 'https://indigodavid.github.io/portfolio/',
+    sourceUrl: 'https://github.com/indigodavid/portfolio',
   },
   {
-    name: "Data Dashboard Healthcare",
-    image: "./images/placeholder-availability.png",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Data Dashboard Healthcare',
+    image: './images/placeholder-availability.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     languages: [
-      "html",
-      "bootstap",
-      "Ruby"
+      'html',
+      'bootstap',
+      'Ruby',
     ],
-    liveUrl: "https://indigodavid.github.io/portfolio/",
-    sourceUrl: "https://github.com/indigodavid/portfolio"
+    liveUrl: 'https://indigodavid.github.io/portfolio/',
+    sourceUrl: 'https://github.com/indigodavid/portfolio',
   },
   {
-    name: "Website Portfolio",
-    image: "./images/placeholder-nature.png",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Website Portfolio',
+    image: './images/placeholder-nature.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     languages: [
-      "html",
-      "bootstap",
-      "Ruby"
+      'html',
+      'bootstap',
+      'Ruby',
     ],
-    liveUrl: "https://indigodavid.github.io/portfolio/",
-    sourceUrl: "https://github.com/indigodavid/portfolio"
+    liveUrl: 'https://indigodavid.github.io/portfolio/',
+    sourceUrl: 'https://github.com/indigodavid/portfolio',
   },
   {
-    name: "Professional Art Printing Data",
-    image: "./images/placeholder-professional.png",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Professional Art Printing Data',
+    image: './images/placeholder-professional.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     languages: [
-      "html",
-      "bootstap",
-      "Ruby"
+      'html',
+      'bootstap',
+      'Ruby',
     ],
-    liveUrl: "https://indigodavid.github.io/portfolio/",
-    sourceUrl: "https://github.com/indigodavid/portfolio"
+    liveUrl: 'https://indigodavid.github.io/portfolio/',
+    sourceUrl: 'https://github.com/indigodavid/portfolio',
   },
   {
-    name: "Data Dashboard Healthcare",
-    image: "./images/placeholder-availability.png",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Data Dashboard Healthcare',
+    image: './images/placeholder-availability.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     languages: [
-      "html",
-      "bootstap",
-      "Ruby"
+      'html',
+      'bootstap',
+      'Ruby',
     ],
-    liveUrl: "https://indigodavid.github.io/portfolio/",
-    sourceUrl: "https://github.com/indigodavid/portfolio"
+    liveUrl: 'https://indigodavid.github.io/portfolio/',
+    sourceUrl: 'https://github.com/indigodavid/portfolio',
   },
   {
-    name: "Website Portfolio",
-    image: "./images/placeholder-nature.png",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Website Portfolio',
+    image: './images/placeholder-nature.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     languages: [
-      "html",
-      "bootstap",
-      "Ruby"
+      'html',
+      'bootstap',
+      'Ruby',
     ],
-    liveUrl: "https://indigodavid.github.io/portfolio/",
-    sourceUrl: "https://github.com/indigodavid/portfolio"
-  }
+    liveUrl: 'https://indigodavid.github.io/portfolio/',
+    sourceUrl: 'https://github.com/indigodavid/portfolio',
+  },
 ];
- 
- class HtmlContents {
 
-  constructor(
-    title = 'Website Portfolio', 
-    description = `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard` 
-    ) {
-    this.title = title;
-    this.description = description;
-  }
-  
-  getH2() {
-    let h2 = document.createElement('h2');
-    h2.innerText = this.title;
-    return h2;
-  }
-  getP() {
-    let p = document.createElement('p');
-    p.innerText = this.description;
-    return p;
-  }
+function getH2(title) {
+  const h2 = document.createElement('h2');
+  h2.innerText = title;
+  return h2;
 }
 
-class Urls {
-  constructor (
-    imageUrl = './images/placeholder-availability.png',  
-    liveUrl = 'https://indigodavid.github.io/portfolio/', 
-    sourceUrl = 'https://github.com/indigodavid/portfolio',
-    imageAlt = 'Project preview') {
-    this.imageUrl = imageUrl;
-    this.liveUrl = liveUrl;
-    this.sourceUrl = sourceUrl;
-    this.imageAlt = imageAlt;
-  }
-
-  getImg() {
-    let img = document.createElement('img');
-    img.src = this.imageUrl;
-    img.alt = this.imageAlt;
-    return img;
-  }
-  
-  getLiveAnchor() {
-    let a = document.createElement('a');
-    a.href = this.liveUrl;
-    a.target = '_blank';
-    a.classList.add('button');
-    a.classList.add('live-icon');
-    a.innerText = 'See Live';
-    return a;
-  }
-
-  getSourceAnchor() {
-    let a = document.createElement('a');
-    a.href = this.sourceUrl;
-    a.target = '_blank';
-    a.classList.add('button');
-    a.classList.add('source-icon');
-    a.innerText = 'See Source';
-    return a;
-  }
+function getP(description) {
+  const p = document.createElement('p');
+  p.innerText = description;
+  return p;
 }
+
+function createModalLink(index) {
+  const anchor = document.createElement('a');
+  anchor.classList.add('button');
+  if (index === 0) {
+    anchor.classList.add('fs');
+    anchor.id = 'featured-link';
+  }
+  anchor.href = `javascript:openModal(${index})`;
+  anchor.innerText = 'See Project';
+  return anchor;
+}
+
+const project = {
+  content: {
+    title: '',
+    description: '',
+  },
+  urls: {
+    imageUrl: '',
+    liveUrl: '',
+    sourceUrl: '',
+    imageAlt: '',
+  },
+  technologies: null,
+  getProjectHtml(index) {
+    const div = document.createElement('div');
+    if (index === 0) {
+      div.classList.add('content');
+      document.getElementById('highlighted-image').src = this.urls.imageUrl;
+    } else {
+      div.classList.add('project');
+      div.classList.add(`project${index}`);
+      div.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.95)), url('${this.urls.imageUrl}')`;
+    }
+    div.appendChild(getH2(this.content.title));
+    div.appendChild(getP(this.content.description));
+    div.appendChild(this.technologies.getUl());
+    div.appendChild(createModalLink(index));
+    return div;
+  },
+};
 
 class Technologies {
-  constructor (techArray = ['html', 'bootstrap', 'Ruby']) {
+  constructor(techArray = ['html', 'bootstrap', 'Ruby']) {
     this.techArray = techArray;
   }
-  getUl () {
-    let ul = document.createElement('ul');
+
+  getUl() {
+    const ul = document.createElement('ul');
     let li;
     ul.classList.add('languages');
-    for(let i = 0; i < this.techArray.length; i++){
+    for (let i = 0; i < this.techArray.length; i += 1) {
       li = document.createElement('li');
       li.innerHTML = this.techArray[i];
       ul.appendChild(li);
@@ -165,61 +163,10 @@ class Technologies {
   }
 }
 
-function createModalLink(index){
-  let anchor = document.createElement('a');
-  anchor.classList.add('button');
-  if (index === 0) {
-    anchor.classList.add('fs');
-  }
-  anchor.href = `javascript:openModal(${index})`;
-  anchor.innerText = 'See Project';
-  return anchor;
-}
-
-let project = {
-  content: null,
-  urls: null,
-  technologies: null,
-  getProjectHtml: function (index) {
-    let div;
-    div = document.createElement('div');
-    if(index === 0){
-      div.classList.add('content');
-      document.getElementById('highlighted-image').src = this.urls.imageUrl;
-    } else {
-      div.classList.add('project');
-      div.classList.add(`project${index}`);
-      div.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.95)), url(' + this.urls.imageUrl + ')';
-    }
-    div.appendChild(this.content.getH2());
-    div.appendChild(this.content.getP());
-    div.appendChild(this.technologies.getUl());
-    div.appendChild(createModalLink(index));
-    return div;
-  }
-}
-
-const projectSection = document.getElementById('projects');
-const featuredContent = document.getElementById('featured-content');
-for(let i = 0; i < projectData.length; i++) {
-  project.content = new HtmlContents(projectData[i].name, projectData[i].description);
-  project.urls = new Urls(projectData[i].image);
-  project.technologies = new Technologies(projectData[i].languages);
-  if(i === 0){
-    featuredContent.appendChild(project.getProjectHtml(i));
-  } else {
-    projectSection.appendChild(project.getProjectHtml(i));
-  }
-}
-
-const modal = document.getElementById('modal-container');
-const popup = document.getElementById('modal');
-const closePopup = document.getElementById('modal-close');
-
 function openModal(index) {
   document.getElementById('modal-title').innerHTML = projectData[index].name;
   document.getElementById('modal-languages').innerHTML = '';
-  let ulLanguages = new Technologies(projectData[index].languages);
+  const ulLanguages = new Technologies(projectData[index].languages);
   document.getElementById('modal-languages').appendChild(ulLanguages.getUl());
   document.getElementById('project-image').src = projectData[index].image;
   document.getElementById('modal-description').textContent = projectData[index].description;
@@ -232,4 +179,17 @@ function closeModal() {
   modal.style.display = 'none';
 }
 
+for (let i = 0; i < projectData.length; i += 1) {
+  project.content.title = projectData[i].name;
+  project.content.description = projectData[i].description;
+  project.urls.imageUrl = projectData[i].image;
+  project.technologies = new Technologies(projectData[i].languages);
+  if (i === 0) {
+    featuredContent.appendChild(project.getProjectHtml(i));
+  } else {
+    projectSection.appendChild(project.getProjectHtml(i));
+  }
+}
+
 closePopup.addEventListener('click', closeModal);
+featuredLink.addEventListener('click', openModal);
