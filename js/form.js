3f31e6f828762f1ae51eb22825f0d24860bf1f5d
David Vera <indigodavid@gmail.com>
2022-05-12 21:24:14 -0500

    Add object template and storage functions

--- js/form.js
+++ js/form.js
@@ -1,7 +1,36 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const formError = document.querySelector('.form-error');
+const fullName = document.getElementById('full-name');
+const message = document.getElementById('message');
 
+const objectData = {
+  name: '',
+  email: '',
+  message: '',
+} 
+
+function storeData() {
+  const data = JSON.stringify(objectData);
+  localStorage.setItem(data);
+}
+
+function fillInputs() {
+  objectData = JSON.parse(localStorage.getItem('data'));
+  fullName.value = objectData.name;
+  email.value = objectData.email;
+  message.innerHTML = objectData.message;
+}
+
+fullName.addEventListener()
+
+if(!localStorage.getItem('data')) {
+  storeData();
+} else {
+  fillInputs();
+}
+
+//LowerCase Validator
 form.addEventListener('submit', (e) => {
   const lowerCaseRegex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
   if (!lowerCaseRegex.test(email.value)) {
@@ -10,3 +39,4 @@ form.addEventListener('submit', (e) => {
     formError.innerHTML = `Email should be written in lower case: ${email.value.toLowerCase()}`;
   }
 });
+
